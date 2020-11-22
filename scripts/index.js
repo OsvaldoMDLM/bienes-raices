55a673b10bc631502bd1e5ad434ce551c6924edf
Osvaldo Medina <62674782+OsvaldoMDLM@users.noreply.github.com>
2020-11-21 22:04:08 -0600

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById('navegacion-mobile')
+nav.style.display = 'none';
+function vernav() 
+{
+    nav.style.display === 'none' ? nav.style.display = 'initial' : nav.style.display = 'none';
+}
